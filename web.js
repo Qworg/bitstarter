f27afc5918605e91669834d30546ba5d4fe88794
Jeff Kramer <jeffkramr@gmail.com>
2013-07-29 06:35:48 +0000

    Updated

--- web.js
+++ web.js
@@ -1,9 +1,15 @@
 var express = require('express');
+var fs = require('fs');
 var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  var resp = new Buffer (100);
+  fs.readFileSync('index.html', function (err, data) {
+    if (err) throw err;
+    resp.write (data, "utf-8");
+  });
+  response.send(resp.toString('utf-8');
 });
 
 var port = process.env.PORT || 5000;
