5be1fcb9e513022748035f2ce08a5f29ee1ed428
Jeff Kramer <jeffkramr@gmail.com>
2013-08-04 03:22:05 +0000

    Fixed my grader.js

--- grader.js
+++ grader.js
@@ -28,7 +28,6 @@ var rest = require('restler');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
 var URLFILE_DEFAULT = "http://google.com";
-var URLFILE = "";
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
@@ -39,30 +38,8 @@ var assertFileExists = function(infile) {
     return instr;
 };
 
-var fetchURLFile = function(inURL) {
-    var url = inURL.toString();
-    var resp = rest.get(inURL).on('complete', function(result) {
-        result;
-    });
-    return resp;
-};
-
-var loadHtmlUrl = function(weburl, callback) {
-    var resultstr = rest.get(weburl).on('complete', function(result) {
-      callback(result.request.res.rawEncode);
-    });
-};
-
-
 var cheerioHtmlFile = function(htmlfile) {
     return cheerio.load(fs.readFileSync(htmlfile));
-};
-
-var cheerioURLFile = function(urlfile) {
-    loadHtmlUrl(urlfile, function(rawEncode) {
-      console.log(rawEncode);
-      return cheerio.load(rawEncode);
-    });
 };
 
 var loadChecks = function(checksfile) {
@@ -80,17 +57,6 @@ var checkHtmlFile = function(htmlfile, checksfile) {
     return out;
 };
 
-var checkURLFile = function(urlfile, checksfile) {
-    $ = cheerioURLFile(urlfile);
-    var checks = loadChecks(checksfile).sort();
-    var out = {};
-    for(var ii in checks) {
-	var present = $(checks[ii]).length > 0;
-        out[checks[ii]] = present;
-    }
-    return out;
-};
-
 var clone = function(fn) {
     // Workaround for commander.js issue.
     // http://stackoverflow.com/a/6772648
@@ -100,16 +66,18 @@ var clone = function(fn) {
 if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
-        .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists))
-	.option('-u, --url <web_file>', 'URL to index.html')
+        .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+	.option('-u, --url <web_file>', 'URL to file to grab')
         .parse(process.argv);
-    var checkJson = null;
-    if (program.file) {
-        checkJson = checkHtmlFile(program.file, program.checks);
-    }
+    
     if (program.url) {
-        checkJson = checkURLFile(program.url, program.checks);
+	rest.get(program.url).on('complete', function(fetchedURL) {
+	    fs.writeFileSync('fetched.html', fetchedURL);
+	    program.file = 'fetched.html';
+	});
     }
+
+    var checkJson = checkHtmlFile(program.file, program.checks);
     var outJson = JSON.stringify(checkJson, null, 4);
     console.log(outJson);
 } else {
